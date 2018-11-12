1b29cf7e5f7c31c1e902a652000df9b487fac1ba
tugg <tugg at zhaw.ch>
2018-11-12 15:46:10 +0100

    add script to scrape metadata from openml

--- ada/open_ml_check_datasets.py
+++ ada/open_ml_check_datasets.py
@@ -41,49 +41,54 @@ for dataset in datasets.keys():
             openml_metadata["regression"] = dict()
             openml_metadata["lc_prediction"] = dict()
 
-
-    dataset_tasks =  [tasks[k] for k in tasks if tasks[k]["did"]==dataset]
     print("Getting Data for Dataset Nr: "+str(dataset))
 
-    dataset_runs = [openml.runs.list_runs(task=[task["tid"]]) for task in dataset_tasks]
-    run_lengths = [len(run) for run in dataset_runs]
-
-    # max runs task
-    task_id = run_lengths.index(max(run_lengths))
-    max_runs = dataset_runs[task_id]
-    runs_to_get = np.random.choice(list(max_runs.keys()), size=min(10,len(max_runs)), replace=False)
+    try:
+        dataset_tasks =  [tasks[k] for k in tasks if tasks[k]["did"]==dataset]
 
 
-    # load runs
-    runs_loaded = openml.runs.get_runs(runs_to_get)
-    # get best runs for dataset
-    print("runs for max. run task loaded")
-    task_type = dataset_tasks[run_lengths.index(max(run_lengths))]["ttid"]
-    print(dataset_tasks[run_lengths.index(max(run_lengths))]["task_type"])
-    if task_type == 1: # classification
-        type_key = "classification"
-        runs_dict = [{"flow":run.flow_name, "score":run.evaluations['area_under_roc_curve'], "run":run} for run in runs_loaded]
-    elif task_type == 2: # regression
-        type_key = "regression"
-        runs_dict = [{"flow":run.flow_name, "score":run.evaluations['mean_absolute_error'], "run":run} for run in runs_loaded]
-    elif task_type == 3: # learning curve pred
-        type_key = "lc_prediction"
-        runs_dict = [{"flow":run.flow_name, "score":run.evaluations['predictive_accuracy'], "run":run} for run in runs_loaded]
-    else:
-        #not iterested in task
-        continue
+        dataset_runs = [openml.runs.list_runs(task=[task["tid"]]) for task in dataset_tasks]
+        run_lengths = [len(run) for run in dataset_runs]
+
+        # max runs task
+        task_id = run_lengths.index(max(run_lengths))
+        max_runs = dataset_runs[task_id]
+        runs_to_get = np.random.choice(list(max_runs.keys()), size=min(200,len(max_runs)), replace=False)
 
 
-    #sort by score
-    runs_dict_sorted = sorted(runs_dict, key=lambda k: k['score'], reverse=False)
+        # load runs
+        runs_loaded = openml.runs.get_runs(runs_to_get)
+        # get best runs for dataset
+        print("runs for max. run task loaded")
+        task_type = dataset_tasks[run_lengths.index(max(run_lengths))]["ttid"]
+        print(dataset_tasks[run_lengths.index(max(run_lengths))]["task_type"])
+        if task_type == 1: # classification
+            type_key = "classification"
+            runs_dict = [{"flow":run.flow_name, "score":run.evaluations['area_under_roc_curve'], "run":run} for run in runs_loaded]
+        elif task_type == 2: # regression
+            type_key = "regression"
+            runs_dict = [{"flow":run.flow_name, "score":run.evaluations['mean_absolute_error'], "run":run} for run in runs_loaded]
+        elif task_type == 3: # learning curve pred
+            type_key = "lc_prediction"
+            runs_dict = [{"flow":run.flow_name, "score":run.evaluations['predictive_accuracy'], "run":run} for run in runs_loaded]
+        else:
+            #not iterested in task
+            continue
 
-    # unique keep-first
-    unique_ind = pa.DataFrame([run["run"].flow_id for run in runs_dict_sorted]).drop_duplicates(subset=None, keep='first', inplace=False).index.values
-    runs_dict_unique = [runs_dict_sorted[i] for i in unique_ind]
 
-    # keep top half
-    if int(len(runs_dict_unique)) > 10:
-        nr_keep = max(int(len(runs_dict_unique) / 4), 10)
-        runs_dict_unique = runs_dict_unique[:nr_keep]
+        #sort by score
+        runs_dict_sorted = sorted(runs_dict, key=lambda k: k['score'], reverse=False)
 
-    openml_metadata[type_key][task_id] = runs_dict_unique
+        # unique keep-first
+        unique_ind = pa.DataFrame([run["run"].flow_id for run in runs_dict_sorted]).drop_duplicates(subset=None, keep='first', inplace=False).index.values
+        runs_dict_unique = [runs_dict_sorted[i] for i in unique_ind]
+
+        # keep top half
+        if int(len(runs_dict_unique)) > 10:
+            nr_keep = max(int(len(runs_dict_unique) / 4), 10)
+            runs_dict_unique = runs_dict_unique[:nr_keep]
+
+        openml_metadata[type_key][task_id] = runs_dict_unique
+
+    except Exception as e:
+        print(e)
---
+++ ada/survey_openml_data.py
@@ -0,0 +1,33 @@
+"""
+Scrape OpenMl for  top runs on each dataset
+"""
+
+import openml
+import os
+import pandas as pa
+import numpy as np
+import pickle
+openml.config.cache_directory = os.path.expanduser('cache_data')
+
+# open and concatenate pickle files
+print("os listdir")
+cache_dir = openml.config.cache_directory
+metadata_pickles = [x for x in os.listdir(cache_dir) if '.pickle' in x]
+
+
+openml_metadata = dict()
+openml_metadata["classification"] = dict()
+openml_metadata["regression"] = dict()
+openml_metadata["lc_prediction"] = dict()
+
+
+for picke_name in metadata_pickles:
+    with open(os.path.join(cache_dir,picke_name), 'rb') as handle:
+        loaded_dict = pickle.load(handle)
+
+        openml_metadata["classification"] = {**openml_metadata["classification"], **loaded_dict["classification"]}
+        openml_metadata["regression"] = {**openml_metadata["regression"], **loaded_dict["regression"]}
+        openml_metadata["lc_prediction"] = {**openml_metadata["lc_prediction"], **loaded_dict["lc_prediction"]}
+
+
+print("asdfsafde")
